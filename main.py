659f28f9e69ab6e3078f12d8701fbc272cae34cf
Barathwaj <Barathwaj1153@gmail.com>
2022-12-15 20:40:01 +0530

    changes

---
+++ main.py
@@ -0,0 +1,19 @@
+import speedtest
+import time
+
+def bytes_to_mb(bytes):
+  KB = 1024 
+  MB = KB * 1024 
+  return int(bytes/MB)
+
+
+s=speedtest.Speedtest()
+print("Tesing....\n")
+
+d= s.download()
+u = s.upload()
+ping = round(s.results.ping)
+
+print(f"Download speed:{bytes_to_mb(d):.2f} Mbps")
+print(f"Upload speed:{bytes_to_mb(u):.2f} Mbps")
+print(f"Ping:{ping:.2f} Ms")
